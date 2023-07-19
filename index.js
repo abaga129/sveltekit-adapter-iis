7b7cc3539f4f244856a5f58f72dcdebbf17a9969
Ethan P. Reker <ethan.reker@wwinc.com>
2023-07-19 14:34:56 -0500

    extend node-adapter

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import fs, { writeFileSync, copyFileSync } from 'fs'
 import { WEB_CONFIG } from './web.config.js'
 import { SERVER_CJS } from './server.cjs.js'
+import node_adapter from '@sveltejs/adapter-node'
 
 /** @type {import('.').default} */
-export default function ({ overrideNodeExePath, overridePort }) {
+export default function (options) {
   /** @type {import('@sveltejs/kit').Adapter} */
+
+  const na = node_adapter(options)
   const adapter = {
     name: 'sveltekit-adapter-iis',
     async adapt(builder) {
+      await na.adapt(builder)
+
       const outputDir = `${builder.config.kit.outDir}/output`
 
       let webConfig = WEB_CONFIG
